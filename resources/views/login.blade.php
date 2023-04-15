07a874ee681db7c728baf6fd69fcce5174606d9f
calvins-simbolon <thecalvins.simbolon@gmail.com>
2023-04-15 13:20:05 +0700

    melakukan migrasi tabel User

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -17,8 +17,8 @@
                 <p class="text-3xl font-bold text-gray-900">
                     Data Report Apps
                 </p>
-                <h1 class="text-lg font-semibold leading-tight tracking-tight text-stone-900">
-                    Sign in to your account
+                <h1 class="text-lg font-semibold leading-tight tracking-tight text-stone-600">
+                    Masuk untuk melihat Data.
                 </h1>
             </div>
             <div class="mx-auto bg-white rounded-lg shadow md:mt-0 sm:max-w-4xl xl:p-0">
@@ -30,7 +30,7 @@
                         <div>
                         <input type="password" name="password" id="password" placeholder="Password" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
                         </div>
-                        <button type="submit" class="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Sign in</button>
+                        <button type="submit" class="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Masuk</button>
                     </form>
                 </div>
             </div>
